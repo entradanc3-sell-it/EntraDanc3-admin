a5e60bd74a3bd9e4c978ad0d089b74369250fff7
ENTRADANC3 <entradanc3@gmail.com>
2025-08-21 21:34:50 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+// === Pegar tu configuración de Firebase entre las líneas START/END ===
+// START
+window.FB_CONFIG = {
+  apiKey: "AIzaSyCDWrSpDwAW3xvlVhv7eIbkGaJA8khSx_M",
+  authDomain: "entradanc3-b2003.firebaseapp.com",
+  projectId: "entradanc3-b2003",
+  storageBucket: "entradanc3-b2003.firebasestorage.app",
+  messagingSenderId: "195691666679",
+  appId: "1:195691666679:web:968632bb77705e861ec7f2",
+  measurementId: "G-777HCN2FPH"
+};
+
+// window.ADMIN_UID = Ov2vTmzXY4ZNcMxqOVOccDp3Hfp2;
+// END
